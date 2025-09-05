06c674cab60da676b4f8e0ca6edfc40c8ed94cd8
Kiyoka Nishiyama <kiyoka@sumibi.org>
2025-09-05 19:12:42 +0900

    バージョンを3.7.0に上げた

--- lisp/sumibi.el
+++ lisp/sumibi.el
@@ -5,7 +5,7 @@
 ;; Copyright (C) 2023 Kiyoka Nishiyama
 ;;
 ;; Author: Kiyoka Nishiyama <kiyoka@sumibi.org>
-;; Version: 3.6.0
+;; Version: 3.7.0
 ;; Keywords: lisp, ime, japanese
 ;; Package-Requires: ((emacs "29.0") (popup "0.5.9") (unicode-escape "1.1") (deferred "0.5.1") (mozc))
 ;; URL: https://github.com/kiyoka/Sumibi
@@ -2451,7 +2451,7 @@ point から行頭方向に同種の文字列が続く間を漢字変換しま�
                                   (length sumibi-history-stack) file-path)))))
 
 (defconst sumibi-version
-  "3.6.0" ;;SUMIBI-VERSION
+  "3.7.0" ;;SUMIBI-VERSION
   )
 (defun sumibi-version (&optional _arg)
   "Sumibiのバージョン番号をミニバッファに表示する.
